b7bdcc5073e92a719aa575604a587798826a3fcf
vituhonorato <vituhonorato.89@gmail.com>
2023-09-05 20:51:10 -0300

    add root/thread-create Page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+import PostThread from "@/components/forms/PostThread";
+import { fetchUser } from "@/lib/actions/user.actions";
 
- 
-export default function Home() {
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  // fetch organization list created by user
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
   return (
-    <div>
-      <h1 className="head-text text-left">Home</h1>
-    </div>
-  )
+    <>
+      <h1 className='head-text'>Create Thread</h1>
+
+      <PostThread userId={userInfo._id} />
+    </>
+  );
 }
+
+export default Page;
