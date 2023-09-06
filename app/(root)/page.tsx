edcb364f0db7be891ffed418c43806a3c45872d0
vituhonorato <vituhonorato.89@gmail.com>
2023-09-06 17:39:13 -0300

    building root/Page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,61 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-import PostThread from "@/components/forms/PostThread";
+import ThreadCard from "@/components/cards/ThreadCard";
+import Pagination from "@/components/shared/Pagination";
+
+import { fetchPosts } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 
-async function Page() {
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
   if (!user) return null;
 
-  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
+
   return (
     <>
-      <h1 className='head-text'>Create Thread</h1>
+      <h1 className='head-text text-left'>Home</h1>
 
-      <PostThread userId={userInfo._id} />
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </>
   );
 }
 
-export default Page;
+export default Home;
